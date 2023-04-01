4c478480d74b517f6b66548851c2f57de7795827
Yusuf Mambrasar <yusuf_mambrasar@yahoo.com>
2023-04-02 05:40:58 +0900

    Add TimeAgoIndonesia function

--- Core/Function.php
+++ Core/Function.php
@@ -180,3 +180,58 @@ if (! function_exists('ParseQuestionFillBlank'))
         call_user_func_array('printf', array_merge((array)$format, $arr)); 
     } 
 }
+
+if (! function_exists('TimeAgoIndonesia')) 
+{
+    function TimeAgoIndonesia($timestamp){  
+        $time_ago = strtotime($timestamp);  
+        $current_time = time();  
+        $time_difference = $current_time - $time_ago;  
+        $seconds = $time_difference;  
+        $minutes      = round($seconds / 60 );        // value 60 is seconds  
+        $hours        = round($seconds / 3600);       //value 3600 is 60 minutes * 60 sec  
+        $days         = round($seconds / 86400);      //86400 = 24 * 60 * 60;  
+        $weeks        = round($seconds / 604800);     // 7*24*60*60;  
+        $months       = round($seconds / 2629440);    //((365+365+365+365+366)/5/12)*24*60*60  
+        $years        = round($seconds / 31553280);   //(365+365+365+365+366)/5 * 24 * 60 * 60  
+        if($seconds <= 60) {  
+        return "Baru saja";  
+        } else if($minutes <=60) {  
+        if($minutes==1){  
+        return "Satu menit lalu";  
+        }else {  
+        return "$minutes menit lalu";  
+        }  
+        } else if($hours <=24) {  
+        if($hours==1) {  
+        return "Satu jam lalu";  
+        } else {  
+        return "$hours jam lalu";  
+        }  
+        }else if($days <= 7) {  
+        if($days==1) {  
+        return "kemarin";  
+        }else {  
+        return "$days hari lalu";  
+        }  
+        }else if($weeks <= 4.3) {  //4.3 == 52/12
+        if($weeks==1){  
+        return "Seminggu lalu";  
+        }else {  
+        return "$weeks minggu lalu";  
+        }  
+        } else if($months <=12){  
+        if($months==1){  
+        return "Sebulan lalu";  
+        }else{  
+        return "$months bulan lalu";  
+        }  
+        }else {  
+        if($years==1){  
+        return "Setahun lalu";  
+        }else {  
+        return "$years tahun lalu";  
+        }  
+        }  
+    } 
+}
