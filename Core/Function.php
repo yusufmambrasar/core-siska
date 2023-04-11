01151b09065dd4ec97fd6ac31b5e19c918e5fa91
Yusuf Mambrasar <yusuf_mambrasar@yahoo.com>
2023-04-11 20:27:45 +0900

    Commit Core Functions

--- Core/Function.php
+++ Core/Function.php
@@ -5,14 +5,14 @@ if (! function_exists('Minifier'))
 {
     function Minifier($code) 
     {
-        // $search = array(
-        //     '/\>[^\S ]+/s',
-        //     '/[^\S ]+\</s',
-        //     '/(\s)+/s',
-        //     '/<!--(.|\s)*?-->/'
-        // );
-        // $replace = array('>', '<', '\\1');
-        // $code = preg_replace($search, $replace, $code);
+        $search = array(
+            '/\>[^\S ]+/s',
+            '/[^\S ]+\</s',
+            '/(\s)+/s',
+            '/<!--(.|\s)*?-->/'
+        );
+        $replace = array('>', '<', '\\1');
+        $code = preg_replace($search, $replace, $code);
         return $code;
     }
 }
@@ -159,122 +159,4 @@ if (! function_exists('RandomString'))
     
         return $RandomString;
     }
-}
-
-if (! function_exists('ParseQuestionFillAnswer')) 
-{
-    function ParseQuestionFillAnswer($format, $arr) 
-    { 
-        $arrs = explode("\n",$arr);
-        $format = str_replace('[....]','<code>%s</code>',$format);
-        call_user_func_array('printf', array_merge((array)$format, $arrs)); 
-    } 
-}
-
-if (! function_exists('ParseQuestionFillBlank')) 
-{
-    function ParseQuestionFillBlank($format, $arr, $answers) 
-    { 
-        $answers = explode("\n",$answers);
-        $arrs = explode("\n",$arr);
-        foreach($arrs as $k => $v)
-        {
-            if(isset($answers[$k]))
-            {
-                $answer = $answers[$k];
-            }
-            else
-            {
-                $answer = '';
-            }
-            $arrs[$k] = '<input type="text" class="form-control form-control-inline" placeholder="..." name="answer_fill'.$k.'" value="'.$answer.'" />';
-        }
-        $format = str_replace('[....]','%s',$format);
-        call_user_func_array('printf', array_merge((array)$format, $arrs)); 
-    } 
-}
-
-if (! function_exists('TimeAgoIndonesia')) 
-{
-    function TimeAgoIndonesia($timestamp){  
-        $time_ago = strtotime($timestamp);  
-        $current_time = time();  
-        $time_difference = $current_time - $time_ago;  
-        $seconds = $time_difference;  
-        $minutes      = round($seconds / 60 );        // value 60 is seconds  
-        $hours        = round($seconds / 3600);       //value 3600 is 60 minutes * 60 sec  
-        $days         = round($seconds / 86400);      //86400 = 24 * 60 * 60;  
-        $weeks        = round($seconds / 604800);     // 7*24*60*60;  
-        $months       = round($seconds / 2629440);    //((365+365+365+365+366)/5/12)*24*60*60  
-        $years        = round($seconds / 31553280);   //(365+365+365+365+366)/5 * 24 * 60 * 60  
-        if($seconds <= 60) 
-        {  
-            return "Baru saja";  
-        } 
-        else if($minutes <=60) 
-        {  
-            if($minutes==1)
-            {  
-                return "Satu menit lalu";  
-            }
-            else
-            {  
-                return "$minutes menit lalu";  
-            }  
-        } 
-        else if($hours <=24) 
-        {  
-            if($hours==1) 
-            {  
-                return "Satu jam lalu";  
-            } 
-                else 
-            {  
-                return "$hours jam lalu";  
-            }  
-        }
-        else if($days <= 7) 
-        {  
-            if($days==1) {  
-                return "Kemarin";  
-            }
-            else 
-            {  
-                return "$days hari lalu";  
-            }  
-        }
-        else if($weeks <= 4.3) //4.3 == 52/12
-        {  
-            if($weeks==1)
-            {  
-                return "Seminggu lalu";  
-            }
-            else 
-            {  
-                return "$weeks minggu lalu";  
-            }  
-        } 
-        else if($months <=12)
-        {  
-            if($months==1)
-            {  
-                return "Sebulan lalu";  
-            }
-            else
-            {  
-                return "$months bulan lalu";  
-            }  
-        }
-        else
-        {  
-            if($years==1)
-            {  
-                return "Setahun lalu";  
-            }
-            else 
-            {  
-                return "$years tahun lalu";  
-            }  
-        }  
-    } 
 }
