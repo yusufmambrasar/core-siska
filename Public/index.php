48702376f53ab5f6ede66e9331d37e42f7142680
Yusuf Mambrasar <yusuf_mambrasar@yahoo.com>
2023-04-25 12:41:12 +0900

    Set Up Core to public folder

---
+++ Public/index.php
@@ -0,0 +1,6 @@
+<?php
+define('BASE',dirname(__DIR__));
+error_reporting(1);
+date_default_timezone_set('Asia/Jayapura');
+require(BASE.'/Core/Core.php');
+$Core = new Core();
--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-define('BASE',dirname(__FILE__));
-error_reporting(1);
-date_default_timezone_set('Asia/Jayapura');
-require(BASE.'/Core/Core.php');
-$Core = new Core();
+header('location: Public/');
